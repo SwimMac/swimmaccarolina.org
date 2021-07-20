b5f9c47b32cdee63540d148819540c495fd54551
rickhocutt <rick.hocutt@gmail.com>
2021-07-20 13:17:36 -0400

    Netlify script

--- src/_assets/scripts/app.js
+++ src/_assets/scripts/app.js
@@ -1,2 +1,11 @@
 console.log(`I was loaded at ${Date(Date.now()).toString()}`);
 
+if (window.netlifyIdentity) {
+  window.netlifyIdentity.on("init", user => {
+    if (!user) {
+      window.netlifyIdentity.on("login", () => {
+        document.location.href = "/admin/";
+      });
+    }
+  });
+}
