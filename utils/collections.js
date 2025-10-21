c25b2043a82616551d5e9f808c6d65ea0caa3f57
Rick Hocutt <rick.hocutt@gmail.com>
2025-10-20 20:50:01 -0400

    Job Sorting

--- utils/collections.js
+++ utils/collections.js
@@ -1,30 +1,40 @@
+const { execSync } = require('child_process');
+
+const lastModifiedCache = new Map();
+
+const getLastModifiedDate = (filePath) => {
+  if (lastModifiedCache.has(filePath)) {
+    return lastModifiedCache.get(filePath);
+  }
+
+  try {
+    const result = execSync(`git log -1 --format=%cI -- "${filePath}"`, {
+      encoding: 'utf8'
+    }).trim();
+
+    const parsedDate = result ? new Date(result) : undefined;
+
+    if (parsedDate && !Number.isNaN(parsedDate.valueOf())) {
+      lastModifiedCache.set(filePath, parsedDate);
+      return parsedDate;
+    }
+  } catch (error) {
+    // ignore and fall through to the fallback
+  }
+
+  const fallback = undefined;
+  lastModifiedCache.set(filePath, fallback);
+  return fallback;
+};
+
+const getSortDate = (item) => {
+  return getLastModifiedDate(item.inputPath) || item.date || new Date(0);
+};
+
 module.exports = {
-  //  tagList: function (collection) {
-  //    let tagSet = new Set();
-  //    collection.getAll().forEach(function (item) {
-  //      if ("tags" in item.data) {
-  //        let tags = item.data.tags;
-
-  //        tags = tags.filter(function (item) {
-  //          switch (item) {
-  //            // this list should match the `filter` list in tags.njk
-  //            case "all":
-  //            case "nav":
-  //            case "post":
-  //            case "posts":
-  //              return false;
-  //          }
-
-  //          return true;
-  //        });
-
-  //        for (const tag of tags) {
-  //          tagSet.add(tag);
-  //        }
-  //      }
-  //    });
-
-  //    // returning an array in addCollection works in Eleventy 0.5.3
-  //    return [...tagSet];
-  //  }
+  employmentByLastEdited(collectionApi) {
+    return collectionApi.getFilteredByTag("employment").sort((a, b) => {
+      return getSortDate(b) - getSortDate(a);
+    });
+  }
 }
